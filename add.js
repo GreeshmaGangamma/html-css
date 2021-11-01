aba173ff55609a8071798cd39359153a0e3c4270
GreeshmaGangamma <greeshma@cronj.com>
2021-11-01 12:35:08 +0530

    Update add.js

--- add.js
+++ add.js
@@ -1,18 +1,19 @@
 function myFunction() {
     var myname = document.getElementById("fname").value;
-    var myphone = document.getElementById("lname").value;
-    var mycontry = document.getElementById("empid").value;
-    var mymail = document.getElementById("pswd").value;
-    var mymail = document.getElementById("srtnum").value;
-    var mymail = document.getElementById("srtname").value;
-    var mymail = document.getElementById("city").value;
-    var mymail = document.getElementById("state").value;
-    var mymail = document.getElementById("zip").value;
-    var mymail = document.getElementById("country").value;
-    result = fname + "," +lname+ "," +empid + ","
+    var lname = document.getElementById("lname").value;
+    var empid = document.getElementById("empid").value;
+    var pswd = document.getElementById("pswd").value;
+    var srtnum = document.getElementById("srtnum").value;
+    var srtname = document.getElementById("srtname").value;
+    var city = document.getElementById("city").value;
+    var state = document.getElementById("state").value;
+    var zip = document.getElementById("zip").value;
+    var country = document.getElementById("country").value;
+   result = myname + "," +lname+ "," +empid + ","
      + pswd+","+srtnum+","+srtname+","+city+","+state+","
     +zip+","+country;
     alert(result);
-    return result;
+    return result ;
   }
+  
   
